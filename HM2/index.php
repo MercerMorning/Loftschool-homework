5165eb8207e3dec65eca1e6bd2f2279541e0f4e0
Nickolay Vasiliev <hugopochta@gmail.com>
2020-05-27 02:23:34 +0300

    ДЗ2

---
+++ HM2/index.php
@@ -0,0 +1,16 @@
+<?php
+require('src/functions.php');
+
+$stringArray = ['Vasya', 'Petya', 'Vova'];
+task1($stringArray);
+echo task1($stringArray, 1);
+
+echo "</br>";
+
+task2('-', 3, 4, 5);
+
+echo "</br>";
+
+task3(5, 5);
+
+
