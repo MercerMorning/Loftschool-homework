e360b3e80dab3500d216d47424e88e532d6c0c81
Nickolay Vasiliev <hugopochta@gmail.com>
2020-05-27 02:23:34 +0300

    ДЗ2

---
+++ HM2/index.php
@@ -0,0 +1,46 @@
+<?php
+require('src/functions.php');
+
+$stringArray = ['Vasya', 'Petya', 'Vova'];
+task1($stringArray);
+echo task1($stringArray, 1);
+
+echo "</br>";
+
+task2('*', 20, 2, 2);
+
+echo "</br>";
+
+task3(5, 5);
+
+echo "</br>";
+echo "<pre>Задание 4.1</pre>";
+
+date_default_timezone_set('Europe/Moscow');
+var_dump(date('d.m.Y H:i'));
+
+echo "</br>";
+echo "<pre>Задание 4.2</pre>";
+
+$time = '24.02.2016 00:00:00';
+echo date('U', strtotime($time));
+
+echo "</br>";
+echo "<pre>Задание 5.1</pre>";
+
+echo str_replace('К', '', 'Карл у Клары украл Кораллы');
+
+echo "</br>";
+echo "<pre>Задание 5.2</pre>";
+
+echo str_replace('Две', 'Три', 'Две бутылки лимонада');
+
+echo "</br>";
+
+$fp = fopen("test.txt", "w");
+
+fwrite($fp, 'Hello again!');
+fclose($fp);
+
+task6('test.txt');
+
---
+++ HM2/src/functions.php
@@ -0,0 +1,80 @@
+<?php
+
+function task1($strings, $isMerge = 0)
+{
+    if ($isMerge) {
+        $result = '';
+        foreach ($strings as $string) {
+             $result .= $string;
+        }
+        return $result;
+    } else {
+        foreach ($strings as $string) {
+            echo "<p>$string</p>";
+        }
+    }
+}
+
+function task2(...$args)
+{
+    if (is_string($args[0])) {
+
+        $result = $args[1];
+
+        for ($i = 2; $i < sizeof($args); $i++) {
+            if (is_int($args[$i]) || is_double($args[$i])) {
+                switch ($args[0]) {
+                    case '+':
+                        $result += $args[$i];
+                        break;
+                    case '-':
+                        $result -= $args[$i];
+                        break;
+                    case '*':
+                        $result *= $args[$i];
+                        break;
+                    case '/':
+                        $result /= $args[$i];
+                        break;
+                    default:
+                        return;
+                }
+
+            } else {
+                return;
+            }
+        }
+    } else {
+        return;
+    }
+    echo $result;
+}
+
+function task3($val1, $val2)
+{
+    if (is_int($val1) && is_int($val2)) {
+        echo "<table>";
+        $p = 0;
+        for ($i = 1; $i <= $val1; $i++) {
+            $p++;
+            echo "<tr>";
+            for ($q = 1; $q <= $val2; $q++) {
+                echo "<td>";
+                echo $q * $p;
+                echo "</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo 'Параметры функции не являются целочисленными';
+    }
+}
+
+function task6($name)
+{
+    $fp = fopen($name, "r");
+    $res = fgets($fp, 1024);
+    fclose($fp);
+    echo $res;
+}
