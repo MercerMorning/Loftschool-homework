e83ffcd6aacd9607d568f1a61b8eaa567708d0b1
Nickolay Vasiliev <hugopochta@gmail.com>
2020-05-29 23:20:25 +0300

    Переделал ДЗ первой недели

---
+++ H1/Task3Constants.php
@@ -0,0 +1,4 @@
+<?php
+const AGE_WORK_MIN = 18;
+const AGE_WORK_MAX = 65;
+const AGE_START = 1;
--- HM2/index.php
+++ HM2/index.php
@@ -1,47 +1,29 @@
 <?php
-require('src/functions.php');
-
-$stringArray = ['Vasya', 'Petya', 'Vova'];
-task1($stringArray);
-echo task1($stringArray, 1);
-
-echo "</br>";
-
-task2('*', 20, 2, 2);
-
-echo "</br>";
-
-task3(5, 5);
-
-echo "</br>";
-echo "<pre>Задание 4.1</pre>";
-
-date_default_timezone_set('Europe/Moscow');
-var_dump(date('d.m.Y H:i'));
-
-echo "</br>";
-echo "<pre>Задание 4.2</pre>";
-
-$time = '24.02.2016 00:00:00';
-echo date('U', strtotime($time));
-
-echo "</br>";
-echo "<pre>Задание 5.1</pre>";
-
-echo str_replace('К', '', 'Карл у Клары украл Кораллы');
-
-echo "</br>";
-echo "<pre>Задание 5.2</pre>";
-
-echo str_replace('Две', 'Три', 'Две бутылки лимонада');
-
-echo "</br>";
-
+include_once ("MainConsts.php");
+include_once("src/functions.php");
+$stringArray = ["Vasya", "Petya", "Vova"];
+echo task1($stringArray),
+MY_EOL,
+task2("*", 20, 0, 2),
+MY_EOL,
+task3(5, 5),
+MY_EOL,
+"Задание 4.1";
+date_default_timezone_set("Europe/Moscow");
+var_dump(date("d.m.Y H:i"));
+echo MY_EOL,
+"Задание 4.2";
+$time = "24.02.2016 00:00:00";
+echo date("U", strtotime($time)),
+MY_EOL,
+"Задание 5.1",
+str_replace("К", "", "Карл у Клары украл Кораллы"),
+MY_EOL, "Задание 5.2",
+str_replace("Две", "Три", "Две бутылки лимонада"),
+MY_EOL;
 $fp = fopen("test.txt", "w");
-
-fwrite($fp, 'Hello again!');
+fwrite($fp, "Hello again!");
 fclose($fp);
-
-task6('test.txt');
+echo task6("test.txt");
 
 
