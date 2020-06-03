87a4dac55af3934b5c1394b7c33f641d312d0e27
MercerMorning <hugopochta@gmail.com>
2020-06-03 23:29:53 +0400

    ДЗ3.2

--- pdo.php
+++ pdo.php
@@ -5,16 +5,16 @@ try {
     echo $e->getMessage();
     die;
 }
-$email = "hugopochta@gmail.com";
-$query = $pdo->prepare("SELECT * FROM users WHERE `email` = :user_email");
-$query->execute(["user_email" => $email]);
-$result = $query->fetchAll()[0]['id'];
-echo $result;
-echo $query->rowCount();
-if ($query->rowCount()){
-    $query = $pdo->prepare("UPDATE users SET `order_count` = `order_count`+1 WHERE `id`=:user_id");
-    $query->execute(["user_id" => $result]);
+$email = $_POST['email'];
+$queryFind = $pdo->prepare("SELECT * FROM users WHERE `email` = :user_email");
+$queryFind->execute(["user_email" => $email]);
+$currentId = $queryFind->fetchAll()[0]['id'];
+if ($queryFind->rowCount()){
+    $queryUpdate = $pdo->prepare("UPDATE users SET `order_count` = `order_count`+1 WHERE `id`=:user_id");
+    $queryUpdate->execute(["user_id" => $currentId]);
 } else {
-    $query = $pdo->prepare("INSERT INTO users (`email`, `order_count`) VALUES (:user_email, 1)");
-    $query->execute(["user_email" => $email]);
+    $querySet = $pdo->prepare("INSERT INTO users (`email`, `order_count`) VALUES (:user_email, 1)");
+    $querySet->execute(["user_email" => $email]);
 }
+echo 'Ваш заказ был оформлен';
+
