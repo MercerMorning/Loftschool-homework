07a2890936ec53a6f3c60f8ac812a1a17d817ed6
MercerMorning <hugopochta@gmail.com>
2020-06-03 23:42:31 +0400

    ДЗ3.2

--- pdo.php
+++ pdo.php
@@ -12,9 +12,15 @@ $currentId = $queryFind->fetchAll()[0]['id'];
 if ($queryFind->rowCount()){
     $queryUpdate = $pdo->prepare("UPDATE users SET `order_count` = `order_count`+1 WHERE `id`=:user_id");
     $queryUpdate->execute(["user_id" => $currentId]);
+    echo 'Ваш заказ был оформлен';
 } else {
-    $querySet = $pdo->prepare("INSERT INTO users (`email`, `order_count`) VALUES (:user_email, 1)");
-    $querySet->execute(["user_email" => $email]);
+    if (preg_match('/^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u', $email)) {
+        $querySet = $pdo->prepare("INSERT INTO users (`email`, `order_count`) VALUES (:user_email, 1)");
+        $querySet->execute(["user_email" => $email]);
+        echo 'Ваш заказ был оформлен';
+    } else {
+        echo "Неправильный email";
+    }
 }
-echo 'Ваш заказ был оформлен';
+
 
